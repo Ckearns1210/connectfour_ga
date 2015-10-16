3badbb7cbc6ece00f10bac297264a8af6b1c7ed5
Colin Kearns <ckearns1210@gmail.com>
2015-10-16 15:59:04 -0400

    Cleaned up JS graveyard of old code

--- app.js
+++ app.js
@@ -3,34 +3,17 @@
 var Cell = function() {
   //value placeholder that will take player value.
   this.value = null;
-  // this.column = col;
-  // this.row = row;
-
 }
 
 Cell.prototype.setState = function(player) {
   this.value === player;
   }
-//
-// Cell.prototype.playable = function() {
-//   if (this.value === null) {
-//     return true;
-//   }
-//   else return false;
-// }
+
 
 Cell.prototype.render = function() {
   return this.value;
 }
 
-// //Column Constructor
-// var Column = function (columnletter, cellsLength) {
-//   for (var i = 0; i < cellsLength; i++) {
-//     if (cellsLength[i].column === columnletter) {
-//       this.colArray.push(cellsLength[i]);
-//     }
-//   }
-// };
 
 //Board constructors
 var Board = function () {
@@ -38,13 +21,6 @@ var Board = function () {
   this.currentPlayer = null;
   this.winner = null;
   this.winnerFound = false;
-//   this.colAArray = [];
-//   this.colBArray = [];
-//   this.colCArray = [];
-//   this.colDArray = [];
-//   this.colEArray = [];
-//   this.colFArray = [];
-//   this.colGArray = [];
  };
 
 //Start Game prototype
@@ -79,8 +55,6 @@ Board.prototype.startGame = function () {
     }
       console.log(this.cellsArray);
   };
-//cellArrayColumn = this.cellArray[i] in click event, the column index that has been clicked
-//makePlay takes in this array.
 
 Board.prototype.makePlay = function(columnArray, currentPlayer) {
   //empty variable to hold the number of spaces occupied
@@ -138,80 +112,3 @@ Board.prototype.makePlay = function(columnArray, currentPlayer) {
 //Board Tester
 b = new Board;
 b.startGame();
-//
-// Board.prototype.makeColumnArrays = function() {
-//   this.colAArray.push(new Column('a', this.cells.length));
-//   this.colBArray.push(new Column('b', this.cells.length));
-//   this.colCArray.push(new Column('c', this.cells.length));
-//   this.colDArray.push(new Column('d', this.cells.length));
-//   this.colEArray.push(new Column('e', this.cells.length));
-//   this.colFArray.push(new Column('f', this.cells.length));
-//   this.colGArray.push(new Column('g', this.cells.length));
-// };
-//
-
- //   for (var i = 0; i < this.cells.length; i++) {
- //     if (this.cells[i].column === 'columnletter') {
- //       this.colAArray.push(this.cells[i]);
- //     }
- //   }
- //
- //   for (var i = 0; i < this.cells.length; i++) {
- //     if (this.cells[i].column === 'b') {
- //       this.colBArray.push(this.cells[i]);
- //     }
- //   }
- //
- //   for (var i = 0; i < this.cells.length; i++) {
- //     if (this.cells[i].column === 'c') {
- //       this.colCArray.push(this.cells[i]);
- //     }
- //   }
- //
- //   for (var i = 0; i < this.cells.length; i++) {
- //     if (this.cells[i].column === 'd') {
- //       this.colDArray.push(this.cells[i]);
- //     }
- //   }
- //
- //   for (var i = 0; i < this.cells.length; i++) {
- //     if (this.cells[i].column === 'e') {
- //       this.colEArray.push(this.cells[i]);
- //     }
- //   }
- //
- //   for (var i = 0; i < this.cells.length; i++) {
- //     if (this.cells[i].column === 'f') {
- //       this.colFArray.push(this.cells[i]);
- //     }
- //   }
- //
- //   for (var i = 0; i < this.cells.length; i++) {
- //     if (this.cells[i].column === 'g') {
- //       this.colGArray.push(this.cells[i]);
- //     }
- //   }
- //
- //   for (var i = 0; i < this.cells.length; i++) {
- //     if (this.cells[i].column === 'h') {
- //       this.colHArray.push(this.cells[i]);
- //     }
- //   }
- // }
- //
- // b.makeColumnArrays();
-
-// //
-// Board.prototype.makePlay = function(columnArray){
-//   for (var i = 0; i < columnArray.length; i++) {
-//       if (columnArray[i].isPlayable()) {
-//         columnArray.splice(i, 1);
-//       }
-//       return columnArray;
-//     }
-// if (columnArray.length > 0) {
-//
-// }
-//creates a Broad obj
-//var b = new Board();
-//b.startGame();
