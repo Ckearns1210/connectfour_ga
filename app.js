a063a826d5f20d4cf7b6afe5030605c537247a7d
Colin Kearns <ckearns1210@gmail.com>
2015-10-17 16:48:03 -0400

    Added Win Logic

--- app.js
+++ app.js
@@ -26,12 +26,12 @@ var Board = function () {
 //Start Game prototype
 Board.prototype.startGame = function () {
   this.cellsArray = [];
-  this.currentPlayer = 1;
+  this.currentPlayer = "blue";
 
 
   console.log('Current Player is Woe 1');
   //Gives me a board of objects with acessible rows and columns
-  //Array with all the cells
+  //Temp Array with all the cells
   var bigArray = [];
   //Temporary Array to store Column Cells
   var columnArray = [];
@@ -40,7 +40,7 @@ Board.prototype.startGame = function () {
     bigArray.push(new Cell());
   }
   console.log(bigArray);
-  //outer loop to repeat the process 7 times, creating 7 column arrays.
+  //outer loop to repeat the below process 7 times, creating 7 column arrays.
   for (var j = 0; j < 7; j++){
     //This iterates 6 times
     for (var i = 0; i < 6; i++) {
@@ -56,10 +56,12 @@ Board.prototype.startGame = function () {
       console.log(this.cellsArray);
   };
 
+//Take two variables, the currentplay, and the clicked Column array from this.cells
 Board.prototype.makePlay = function(columnArray, currentPlayer) {
   //empty variable to hold the number of spaces occupied
   var occupiedSpaces = 0;
   //Goes through the array, checks if each cell object is occupied, if it is, it adds to the variable.
+  //This will be used both to check if the column is playable, and also to choose where to place the piece.
   for (var i = 0; i < columnArray.length; i++) {
     if (columnArray[i].value != null) {
       occupiedSpaces += 1
@@ -77,23 +79,23 @@ Board.prototype.makePlay = function(columnArray, currentPlayer) {
          piecePlacer = 5;
          columnArray[piecePlacer].value = currentPlayer;
          break;
-         case occupiedSpaces = 1:
+         case 1:
          piecePlacer = 4;
          columnArray[piecePlacer].value = currentPlayer;
          break;
-         case occupiedSpaces = 2:
+         case 2:
          piecePlacer = 3;
          columnArray[piecePlacer].value = currentPlayer;
          break;
-         case occupiedSpaces = 3:
+         case 3:
          piecePlacer = 2;
          columnArray[piecePlacer].value = currentPlayer;
          break;
-         case occupiedSpaces = 4:
+         case 4:
          piecePlacer = 1;
          columnArray[piecePlacer].value = currentPlayer;
          break;
-         case occupiedSpaces = 5:
+         case 5:
          piecePlacer = 0;
          columnArray[piecePlacer].value = currentPlayer;
          break;
@@ -104,20 +106,63 @@ Board.prototype.makePlay = function(columnArray, currentPlayer) {
     //If column is full, alert you can't place here.
     else alert('You can\'t just shove a piece in here, it is full!');
 
-    switch(this.currentPlayer){
-          case 1:
-          this.currentPlayer = 2;
-          break;
-          case 2:
-          this.currentPlayer = 1;
-          break;
-        }
+    // switch(this.currentPlayer){
+    //       case 1:
+    //       this.currentPlayer = 2;
+    //       break;
+    //       case 2:
+    //       this.currentPlayer = 1;
+    //       break;
+    //     }
 };
+//array location is equal to this.cellsArray[col][row]
+Board.prototype.checkWinner = function (board) {
+  var board = this.cellsArray
+  // console.log(board);
+//Check horizontal
+    for (var r = 0; r < 4; r++) {
+      for (var c = 0; c < 6; c++) {
+        // console.log(((board[r][c].value === board[r+1][c].value) && (board[r+1][c].value === board[r+2][c].value) && (board[r+2][c].value === board[r+3][c].value)))
+        if ((board[r][c].value === board[r+1][c].value) && (board[r+1][c].value === board[r+2][c].value) && (board[r+2][c].value === board[r+3][c].value)) {
+          if(board[r][c].value !== null) {
+          return 'is the winner!';
+        }
+      }
+    }
+  }
+  // Check Vertical
+  for (var r = 0; r < 7; r++) {
+    for (var c = 0; c < 3; c++) {
+      if ((board[r][c].value === board[r][c+1].value) && (board[r][c+1].value === board[r][c+2].value) && (board[r][c+2].value === board[r][c+3].value)) {
+        if(board[r][c].value !== null) {
+          return 'is the winner!';
+        }
+      }
+    }
+  };
 
-Board.prototype.checkWin = function () {
-  
-}
+  //Check Diagonal Left to Right
+    for (var r = 0; r < 4; r++) {
+      for (var c = 0; c < 3; c++) {
+        if ((board[r][c].value === board[r+1][c+1].value) && (board[r+1][c+1].value === board[r+2][c+2].value) && (board[r+2][c+2].value === board[r+3][c+3].value))  {
+          if(board[r][c].value !== null) {
+            return 'is the winner!';
+          }
+        }
+      }
+    };
 
+  //  Check Diagonal Right to Left
+      for (var r = 3; r < 6; r++) {
+        for (var c = 0; c < 3; c++) {
+          if ((board[r][c].value === board[r-1][c+1].value) && (board[r-1][c+1].value === board[r-2][c+2].value) && (board[r-2][c+2].value === board[r-3][c+3].value)) {
+            if(board[r][c].value !== null) {
+              return 'is the winner!';
+          }
+        }
+      }
+    }
+  }
 
 
 
@@ -127,3 +172,7 @@ Board.prototype.checkWin = function () {
 //Board Tester
 b = new Board;
 b.startGame();
+// b.makePlay(b.cellsArray[5], "blue");
+// b.makePlay(b.cellsArray[4], "blue");
+// b.makePlay(b.cellsArray[3], "blue");
+// b.makePlay(b.cellsArray[2], "blue");
